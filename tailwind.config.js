ea8ce4c5011a44dafbd281872474a657a6729a8b
Matheus <matheus.p.soares242@gmail.com>
2024-10-16 17:06:33 -0300

    start project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.html',
+    './app/scripts/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
